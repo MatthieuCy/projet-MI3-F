fa6426afe15a453c57464c80f75ba6ff07cfbeb5
paulo4895 <vinzantpaularthur@gmail.com>
2025-04-11 14:20:02 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX_CARAC=50
 
 typedef struct{
-char NOM[MAX_CARAC];
+char* NOM;
 char* ESPECE;
 int ANNEE;
 char* COMMENTAIRE;
-int NUM;
+int NUM
+int poids;
 }Animal;
 
-void MENU(){
+void MENU(Refuge* a){
   int c;
-  printf("quelle critère recherchez vous?\n");
-  printf("1-A l'aide du nom \n 2-A l'aide de l'èspece \n 3-A l'aide de l'âge \n");
-  do{
-    scanf(%d, c);
-      if 
+  char* b;
+  printf("donner le nom de l'animal,sinon ecrire 0 \n");
+  scanf("%s, b");
+    switch(b){
+      case'chat':
+    
   }
-  switch
-}
 
 
