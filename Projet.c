bd2d8f6cc7999609fb3ea8669e4a683b1a6ed3e2
Andy00006 <andy.laloum8@gmail.com>
2025-04-11 15:16:14 +0200

    Update Projet.c

--- Projet.c
+++ Projet.c
@@ -1,37 +1,80 @@
-#define SIZE 50
+#include <stdio.h>
+#include <string.h>
 
-int affiche_animaux(Animal refuge){
+typedef struct {
+    char nom[50];
+} Animal;
 
-  int i = 0, c = 0;
-  int chien = 0, chat = 0, autruche = 0, hamster = 0;
+void affiche_animaux(Animal refuge[50]) {
+    int i, c = 0;
+    int chien = 0, chat = 0, autruche = 0, hamster = 0;
 
-  for(i; i <= 50; i++){
-    if(strlen(refuge[i].nom) != 0){
-      c++;
+    // Comptage du nombre d'animaux présents dans le refuge
+    for (i = 0; i < 50; i++) {
+        if (strlen(refuge[i].nom) != 0) {
+            c++;
+        }
     }
-  }
-  printf("Il y a %d anilaux dans le refuge.\n", c);
+    printf("Il y a %d animaux dans le refuge.\n", c);
 
-  for(i = 0, i <= c; i++){
-
-    if(refuge[i].nom == 'chien'){
-      chien++;
+    // Comptage du nombre d'animaux par espèce
+    for (i = 0; i < 50; i++) {
+        if (strcmp(refuge[i].nom, "chien") == 0) {
+            chien++;
+        }
+        if (strcmp(refuge[i].nom, "chat") == 0) {
+            chat++;
+        }
+        if (strcmp(refuge[i].nom, "autruche") == 0) {
+            autruche++;
+        }
+        if (strcmp(refuge[i].nom, "hamster") == 0) {
+            hamster++;
+        }
     }
 
-    if(refuge[i].nom == 'chat'){
-      chat++;
+    // Tableau pour stocker les animaux et leur nombre
+    AnimalCount animaux[4] = {
+        {"chien", chien},
+        {"chat", chat},
+        {"autruche", autruche},
+        {"hamster", hamster}
+    };
+
+    // Tri des animaux par nombre décroissant
+    for (i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            if (animaux[i].count < animaux[j].count) {
+                // Échange des animaux
+                AnimalCount temp = animaux[i];
+                animaux[i] = animaux[j];
+                animaux[j] = temp;
+            }
+        }
     }
 
-    if(refuge[i].nom == 'autruche'){
-      autruche++;
+    // Affichage du nombre d'animaux par espèce
+    printf("\nNombre d'animaux par espèce :\n");
+    for (i = 0; i < 4; i++) {
+        if (animaux[i].count > 0) {
+            printf("%s: %d\n", animaux[i].espece, animaux[i].count);
+        }
     }
+}
 
-    if(refuge[i].nom == 'hamster'){
-      hamster++;
-    }
+
+
+void affiche_nouritture(Animal refuge[50]){
 
 int main(){
 
-  Animal refuge[SIZE];
+  Animal * refuge = malloc(sizeof(Animal) * SIZE);
+  if(refuge == NULL){
+    printf("Erreur d'allocation mémoire");
+    exit(1);
+  }
+
+  return 0;
+}
     
     
