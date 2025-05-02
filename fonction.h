eab43dbda80301007f2591f53e3fbea92cb264e5
Andy00006 <andy.laloum8@gmail.com>
2025-05-02 14:48:57 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -3,5 +3,7 @@
 
 #include "animal.h"
 
-void calculer_nettoyage_hebdo(const Animal* animaux, int nb_animaux);
-void afficher_nombre_par_espece(const Animal* animaux, int nb_animaux);
+void calculer_nettoyage_hebdo( Animal* animaux, int nb_animaux);
+void affiche_animaux(Animal *animaux);
+void affiche_nourriture(Animal *animaux);
+void afficher_par_tranche_age( Animal* animaux, int nb_animaux);
