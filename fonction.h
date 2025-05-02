d318dee8dd723df84175e697eae30d4ede692443
Andy00006 <andy.laloum8@gmail.com>
2025-05-02 14:32:58 +0200

    Create fonction.h

---
+++ fonction.h
@@ -0,0 +1,7 @@
+#ifndef STATISTIQUES_H
+#define STATISTIQUES_H
+
+#include "animal.h"
+
+void calculer_nettoyage_hebdo(const Animal* animaux, int nb_animaux);
+void afficher_nombre_par_espece(const Animal* animaux, int nb_animaux);
