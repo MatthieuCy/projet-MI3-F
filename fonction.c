5ec968853fdb631b37645e632de5213ae8712e85
MatthieuCy <matthieu.dublanc@gmail.com>
2025-05-08 10:15:41 +0200

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -164,10 +164,15 @@ void afficher_par_tranche_age( Animal* animaux, int nb_animaux) {
 
 void menu() {
     printf("\n=== MENU REFUGE ANIMALIER ===\n");
-    printf("1. Rechercher un/des animaux\n");
+   printf("1. Rechercher un animal\n");
     printf("2. Ajouter un animal\n");
-    printf("3. Adoption d’un animal\n");
+    printf("3. Adopter un animal\n");
+    printf("4. Inventaire par espèce\n");
+    printf("5. Inventaire par tranche d'âge\n");
+    printf("6. Besoin en croquettes\n");
+    printf("7. Charge de nettoyage\n");
     printf("0. Quitter\n");
+    printf("Votre choix : ");
 }
 
 
