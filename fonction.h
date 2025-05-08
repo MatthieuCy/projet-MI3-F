02babd206898d16410640e3b1867f2a8908edfb3
MatthieuCy <matthieu.dublanc@gmail.com>
2025-05-08 11:06:46 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -7,11 +7,7 @@ void calculer_nettoyage_hebdo( Animal* animaux, int nb_animaux);
 void affiche_animaux(Animal *animaux);
 void affiche_nourriture(Animal *animaux);
 void afficher_par_tranche_age( Animal* animaux, int nb_animaux);
-void menu();
-void ajouter_animal();
-void rechercher_animaux();
-void adopter_animal();
-void afficher_animal(Animal a);
-int type_age(int age);
+
+
 
 #endif
