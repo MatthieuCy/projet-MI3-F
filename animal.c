a79915774514088169a4025618449bc8f69a8199
MatthieuCy <matthieu.dublanc@gmail.com>
2025-05-08 12:32:41 +0200

    Update animal.c

--- animal.c
+++ animal.c
@@ -17,3 +17,37 @@ void afficher_animal(Animal a) {
     printf("ID: %d\nNom: %s\nEspèce: %s\nAnnée: %d\nPoids: %.2f kg\nCommentaire: %s\n",
            a.id, a.nom, a.espece, a.annee_naissance, a.poids, a.commentaire);
 }
+
+
+int charger_animaux(const char* nom_fichier, Animal* animaux, int max) {
+    FILE* f = fopen(nom_fichier, "r");
+    if (!f) return 0;
+
+    int count = 0;
+    char ligne[512];
+    while (fgets(ligne, sizeof(ligne), f) && count < max) {
+        Animal a;
+        //remplir un animal
+        if (sscanf(ligne, "%d;%[^;];%[^;];%d;%f;%[^\n]",
+                   &a.id, a.nom, a.espece, &a.annee_naissance, &a.poids, a.commentaire) == 6) {
+            animaux[count++] = a;
+        }
+    }
+
+    fclose(f);
+    return count;
+}
+
+void sauvegarder_animaux(const char* nom_fichier, Animal* animaux, int nb) {
+    FILE* f = fopen(nom_fichier, "w");
+    if (!f) return;
+
+    for (int i = 0; i < nb; i++) {
+     //affichage dans le termianl de tous les données des animaux
+        fprintf(f, "%d;%s;%s;%d;%.2f;%s\n",
+                animaux[i].id, animaux[i].nom, animaux[i].espece,
+                animaux[i].annee_naissance, animaux[i].poids, animaux[i].commentaire);
+    }
+
+    fclose(f);
+}
