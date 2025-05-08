a5d22b3629abd490bb1d2fe84106ba1dfbe2c15f
MatthieuCy <matthieu.dublanc@gmail.com>
2025-05-08 11:20:08 +0200

    Delete fichier.h

--- fichier.h
+++
@@ -1,9 +0,0 @@
-#ifndef FICHIER_H
-#define FICHIER_H
-
-#include "animal.h"
-
-int charger_animaux( char* nom_fichier, Animal* animaux, int max);
-void sauvegarder_animaux( char* nom_fichier, Animal* animaux, int nb);
-
-#endif
