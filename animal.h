2cf06cbd3f88703568062f1af59874176614dd3b
Andy00006 <andy.laloum8@gmail.com>
2025-05-09 14:37:13 +0200

    Update animal.h

--- animal.h
+++ animal.h
@@ -1,23 +1,24 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-#define MAX_NOM 50
-#define MAX_ESPECE 30
-#define MAX_COMMENTAIRE 200
+
+#define TAILLE_MAX 100
+#define NOM_TAILLE 50
+#define COMMENTAIRE_TAILLE 100
+#define FICHIER_DATA "animaux/data.txt"
 
 typedef struct {
-    int id;
-    char nom[MAX_NOM];
-    char espece[MAX_ESPECE];   // Chien, Chat, etc.
-    int annee_naissance;
-    float poids;
-    char commentaire[MAX_COMMENTAIRE];
+    char nom[NOM_TAILLE];
+    int age;
+    char race[NOM_TAILLE];
+    char genre;
+    char commentaire[COMMENTAIRE_TAILLE];
 } Animal;
 
-// Fonctions
-Animal creer_animal(int id, const char* nom, const char* espece, int annee, float poids, const char* commentaire);
-
 void afficher_animal(Animal a);
+void ajouter_animal(Animal* animaux, int* nb_animaux);
+int charger_animaux(const char* nom_fichier, Animal* animaux, int max);
+void enregistrer_animaux(const char* nom_fichier, Animal* animaux, int nb_animaux);
+void afficher_animaux(Animal* animaux, int nb_animaux);
+void rechercher_animaux(Animal* animaux, int nb_animaux);
 
-void charger_animaux(Animal* animaux, int* nb_animaux);
-void sauvegarder_animaux(Animal* animaux, int nb_animaux);
 #endif
