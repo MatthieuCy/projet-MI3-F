2eec2c096f691e0ab7ddc25575439b585dcea74b
Andy00006 <andy.laloum8@gmail.com>
2025-05-11 14:33:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "animal.h"
 
 int main() {
@@ -6,6 +7,8 @@ int main() {
     int nb_animaux = charger_animaux(FICHIER_DATA, animaux, TAILLE_MAX);
 
     int choix;
+    char buffer[10];
+
     do {
         printf("\n=== Menu du refuge ===\n\n");
         printf("1. Ajouter un animal\n");
@@ -13,8 +16,21 @@ int main() {
         printf("3. Adopter un animal\n");
         printf("4. Afficher l'inventaire des animaux\n");
         printf("5. Quitter\n\n");
-        printf("Choisissez une option : ");
-        scanf("%d", &choix);
+
+        // Lecture sécurisée du choix
+        do {
+            printf("Choisissez une option (1-5) : ");
+            fgets(buffer, sizeof(buffer), stdin);
+            buffer[strcspn(buffer, "\n")] = '\0';
+
+            if (strlen(buffer) == 1 && buffer[0] >= '1' && buffer[0] <= '5') {
+                choix = buffer[0] - '0';
+                break;
+            } else {
+                printf("❌ Entrée invalide. Veuillez choisir un nombre entre 1 et 5.\n");
+            }
+        } while (1);
+
         printf("\n");
 
         switch (choix) {
@@ -24,24 +40,22 @@ int main() {
             case 2:
                 rechercher_animaux(animaux, nb_animaux);
                 break;
-            case 3:
-                {
-                    int id_a_adopter;
-                    printf("\nEntrez l'ID de l'animal à adopter : ");
-                    scanf("%d", &id_a_adopter);
-                    adopter_animal_par_id(animaux, &nb_animaux, id_a_adopter);
-                }
+            case 3: {
+                int id_a_adopter;
+                printf("\nEntrez l'ID de l'animal à adopter : ");
+                scanf("%d", &id_a_adopter);
+                adopter_animal_par_id(animaux, &nb_animaux, id_a_adopter);
                 break;
+            }
             case 4:
                 afficher_inventaire(animaux, nb_animaux);
                 break;
             case 5:
                 enregistrer_animaux(FICHIER_DATA, animaux, nb_animaux);
-                printf("Données enregistrées. Au revoir !\n");
+                printf("✅ Données enregistrées. Au revoir !\n");
                 break;
-            default:
-                printf("Choix invalide.\n");
         }
+
     } while (choix != 5);
 
     return 0;
